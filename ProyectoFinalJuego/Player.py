31b97749ae6da9539344c75189e55025cfb2a5c6
pilarsdiaz <39840315+pilarsdiaz@users.noreply.github.com>
2020-05-13 20:41:40 -0400

    Update Player.py

--- ProyectoFinalJuego/Player.py
+++ ProyectoFinalJuego/Player.py
@@ -1,10 +1,15 @@
 import  pygame
 class Player:
     def __init__(self):
+        #Creacion de la nave o jugador con una imagen
         self.playerImg=pygame.image.load('img/nave.png')
+        #Tamaño para la deteccion de colicion
         self.tam=32
+        #Posicion en donde iniciara la nave
         self.pos=[370,540]
+        #Velocidad a la ira la nave 
         self.vel=2
+     #Movimieto de la nave en la pantalla
     def move_player(self,ancho,height):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and self.pos[0] > 0:
@@ -15,6 +20,6 @@ class Player:
             self.pos[1]-=self.vel
         if keys[pygame.K_DOWN] and self.pos[1] < 540:
             self.pos[1]+= self.vel
-
+    #Actualizacion de la nave en la Pantalla
     def player_update(self,screem):
         screem.blit(self.playerImg,(self.pos[0],self.pos[1]))
