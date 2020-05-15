096d769da9f389da068d8fdf0fb4b1012dec0489
pilarsdiaz <39840315+pilarsdiaz@users.noreply.github.com>
2020-05-15 12:10:30 -0400

    Update Main.py

--- ProyectoFinalJuego/Main.py
+++ ProyectoFinalJuego/Main.py
@@ -2,32 +2,40 @@
 import pygame
 import sys
 from Player import Player #Clase de jugador
-from Screen import screen #Clase de la pantalla
+from Screen import Screen #Clase de la pantalla
 from Meteor import Meteor # Clase del meteoro
 
 pygame.init()
 #Crea la ventana, el jugador y a los meteritos 
 ventana=screen()
-ventana.init_TitleIcon()
+ventana.init_title_icon()
 scr=pygame.display.set_mode((ventana.width,ventana.heigh))
 jugador=Player()
 meteoro=Meteor()
 
-def  detectar_colision(j,m):
-    # Esta funcione es la que dectecta las coliciones entre la nave y el meteorito
-    jx=j.pos[0]
-    jy=j.pos[1]
-    for i in range(len(m.meteorImg)):
-        if (m.posx[i]>=jx and m.posx[i]<(jx+j.tam)) or (jx>=m.posx[i] and jx <(m.posx[i]+m.tam)):
-            if (m.posy[i] >= jy and m.posy[i]< (jy + j.tam)) or (jy >= m.posy[i] and jy < (m.posy[i] + m.tam)):
-                return True
-    return False
+class Utils:
+    
+    @classmethod
+    def  detectar_colision(j,m):
+        # Esta funcione es la que dectecta las coliciones entre la nave y el meteorito
+        jx=j.pos[0]
+        jy=j.pos[1]
+        for i in range(len(m.meteorImg)):
+            choque_posicion_meteorito_x = m.posx[i]>=jxand m.posx[i]<(jx+j.tam)
+            choque_posicion_jugador_x = jx>=m.posx[i] and jx <(m.posx[i]+m.tam)
+            choque_posicion_meteorito_y = m.posy[i] >= jy and m.posy[i]< (jy + j.tam)
+            choque_posicion_jugador_y = jy >= m.posy[i] and jy < (m.posy[i] + m.tam)
+                
+            if choque_posicion_meteorito_x orchoque_posicion_jugador_x :
+                if  choque_posicion_meteorito_y or choque_posicion_jugador_y:
+                    return True
+        return False
 
 
 game_over=False
 # El ciclo donde el juego inicializa 
 while not game_over:
-    ventana.update_Background(scr)
+    ventana.update_background(scr)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -38,6 +46,6 @@ while not game_over:
     meteoro.meteor_update(scr)
     jugador.player_update(scr)
     #detecta la colicion y acaba el juego 
-    if detectar_colision(jugador,meteoro):
+    if Utils.detectar_colision(jugador,meteoro):
         game_over=True
     pygame.display.update()
