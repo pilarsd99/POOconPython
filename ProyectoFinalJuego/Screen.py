e3ef406e8c1d2b91693a3c7f05a3b7fe4319c57d
pilarsdiaz <39840315+pilarsdiaz@users.noreply.github.com>
2020-05-15 11:56:47 -0400

    Update Screen.py

--- ProyectoFinalJuego/Screen.py
+++ ProyectoFinalJuego/Screen.py
@@ -1,7 +1,7 @@
 import pygame
 
-class screen:
-    def __init__(self
+class Screen:
+    def __init__(self):
         #Creacion del tamaño, Titulo, Icono y el fondo de patalla para el juego 
         self.width =800
         self.heigh=600
@@ -9,9 +9,9 @@ class screen:
         self.icon=pygame.image.load('img/iconspace.png')
         self.background=pygame.image.load('img/Background.jpg')
     #Esta funcion carga el titulo e Icono de la pantalla
-    def init_TitleIcon(self):
+    def init_title_icon(self):
         pygame.display.set_caption(self.title)
         pygame.display.set_icon(self.icon)
      #Actualizacion de la pantalla 
-    def update_Background(self,ventana):
+    def update_background(self,ventana):
         ventana.blit(self.background,(0,0))
